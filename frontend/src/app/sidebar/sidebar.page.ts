a1da2dbc925f93a121debe334aa29dc1c6970038
AnilaDuka <aniladukaa@gmail.com>
2025-01-14 23:06:27 +0100

    style changes and sorting

--- frontend/src/app/sidebar/sidebar.page.ts
+++ frontend/src/app/sidebar/sidebar.page.ts
@@ -1,16 +1,31 @@
-import { Component, inject  } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
   templateUrl: './sidebar.page.html',
-  styleUrl: './sidebar.page.scss'
+  styleUrls: ['./sidebar.page.scss']
 })
 export class SidebarPage {
   private router = inject(Router);
+  activePath: string = '';
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.activePath = this.router.url;
+    });
+
+    this.activePath = this.router.url;
+  }
 
   goToPage(path: string) {
     this.router.navigate([path]);
+  }
+
+  isActive(path: string): boolean {
+    return this.activePath === path;
   }
 }
