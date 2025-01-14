f9a046f92fbd7d02374057485975e5fd6c39def8
AnilaDuka <aniladukaa@gmail.com>
2025-01-14 22:05:24 +0100

    Reverted httpclient import and other changes

--- frontend/src/app/employee/employee.page.ts
+++ frontend/src/app/employee/employee.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+// import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { EmployeeService } from '../services/employee.service';
 import { Router } from '@angular/router';
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [
     CommonModule,
-    HttpClientModule
+    // HttpClientModule
   ],
   providers: [EmployeeService],
   templateUrl: './employee.page.html',
@@ -19,6 +19,8 @@ export class EmployeePage implements OnInit {
   employees: any[] = [];
   private employeeService = inject(EmployeeService);
   private router = inject(Router)
+  sortDirection: 'asc' | 'desc' = 'asc';
+  sortColumn: string = '';
 
   openEditModal(employeeId: number) {
     this.router.navigate(['/create-employee', employeeId]);
@@ -68,4 +70,36 @@ export class EmployeePage implements OnInit {
   sendToCreateEmployee() {
     this.router.navigate(['/create-employee'])
   }
+
+  sortEmployees(column: string): void {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+  
+    this.employees.sort((a, b) => {
+      let valueA = a[column];
+      let valueB = b[column];
+  
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        if (this.sortDirection === 'asc') {
+          return valueA - valueB;
+        } else {
+          return valueB - valueA;
+        }
+      }
+  
+      valueA = valueA ? valueA.toString().toLowerCase() : '';
+      valueB = valueB ? valueB.toString().toLowerCase() : '';
+      
+      if (this.sortDirection === 'asc') {
+        return valueA < valueB ? -1 : 1;
+      } else {
+        return valueA > valueB ? -1 : 1;
+      }
+    });
+  }
+  
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-// import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    // provideHttpClient(),
-    CookieService
+    provideHttpClient(), // Correctly include provideHttpClient
   ],
 }).catch((err) => console.error(err));
